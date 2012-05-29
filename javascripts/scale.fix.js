76a41cb80a8ba14331eda529a0d7053297ef6ebe
patmeeker <patmeeker@gmail.com>
2012-05-29 11:40:47 -0700

    Update javascripts/scale.fix.js

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -1,17 +1,58 @@
-var metas = document.getElementsByTagName('meta');
-var i;
-if (navigator.userAgent.match(/iPhone/i)) {
-  for (i=0; i<metas.length; i++) {
-    if (metas[i].name == "viewport") {
-      metas[i].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
-    }
-  }
-  document.addEventListener("gesturestart", gestureStart, false);
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+
+
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=275548259187268";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
+var anchor = '',
+    pageOffset = '',
+    scrollPos = '';
+$(function() {
+    $("a[href*='#']").click(function(e) {
+        e.preventDefault();
+        anchor = $(this).attr('href');
+  	if (anchor == "#download") {
+		    viewOffset = 600;
+			}
+			if (anchor == "#about") {
+		    viewOffset = 120;
+			}
+			if (anchor == "#faq") {
+		    viewOffset = 140;
+			}
+        pageOffset = $("a[name='" + anchor + "']").offset();
+        scrollPos = pageOffset.top - viewOffset
+        $('html, body').animate({
+            scrollTop: scrollPos
+        }, '50');
+    });
+});
+
+function hideDF(){
+$('#rose').hide();
+$('#kevin').hide();
+$('#jack').show();
 }
-function gestureStart() {
-  for (i=0; i<metas.length; i++) {
-    if (metas[i].name == "viewport") {
-      metas[i].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
-    }
-  }
+
+function hideDA(){
+$('#jack').hide();
+$('#kevin').hide();
+$('#rose').show();
+}
+
+function hideFA(){
+$('#jack').hide();
+$('#rose').hide();
+$('#kevin').show();
 }
