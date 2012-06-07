135b6e1a879bc976fd8f7e3e66a92d024e637ac2
patmeeker <patmeeker@gmail.com>
2012-06-07 16:47:14 -0700

    Update gh-pages

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -39,6 +39,19 @@ $(function() {
     });
 });
 
+$window.scroll(function() {
+    var scrollTop = $window.scrollTop();
+    if(scrollTop > 0 && scrollTop < 800) {
+        hideDF();
+    } else if(scrollTop > 800 && scrollTop < 1300) {
+        hideDA();
+    } else if(scrollTop > 1300 && scrollTop < 6300) {
+        hideFA();
+    }
+});​
+
+
+
 function hideDF(){
 $('#rose').hide();
 $('#kevin').hide();
